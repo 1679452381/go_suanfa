ed551c98428c23d2313a8879286cbf540f2491b2
zxc <15660589213@163.com>
2023-05-04 09:51:25 +0800

    移除链表元素

---
+++ suanfa/listNode/1.链表的定义.go
@@ -0,0 +1,24 @@
+/**
+ * @author X
+ * @date 2023/5/4
+ * @description
+ **/
+package main
+
+//type ListNode struct {
+//	Value int
+//	Next  *ListNode
+//}
+//
+//func main() {
+//	a := &ListNode{
+//		1,
+//		nil,
+//	}
+//	b := &ListNode{
+//		2,
+//		a,
+//	}
+//	fmt.Println(b)
+//	fmt.Println(b.Next)
+//}
