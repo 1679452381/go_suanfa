71eb1b2337f80c2e5d64af283e0235bf18f88a37
zxc <15660589213@163.com>
2023-05-07 23:30:10 +0800

    组合:剪枝优化

--- suanfa/Backtracking/1.组合.go
+++ suanfa/Backtracking/1.组合.go
@@ -18,6 +18,11 @@ func back(ret *[][]int, path []int, n, k, start int) {
 		*ret = append(*ret, temp)
 		return
 	}
+	//剪枝优化
+	//已经选择的元素个数：path.size();
+	//所需需要的元素个数为: k - path.size();
+	//列表中剩余元素（n-i） >= 所需需要的元素个数（k - path.size()）
+	//在集合n中至多要从该起始位置 : i <= n - (k - path.size()) + 1，开始遍历
 	for i := start; i <= n-(k-len(path))+1; i++ {
 		//for i := start; i <= n; i++ {
 		path = append(path, i)
