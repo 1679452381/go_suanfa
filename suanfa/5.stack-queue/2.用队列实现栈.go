be9c55dd7a3c57b4a6fdbfa5c3470ddd20af3af8
zxc <15660589213@163.com>
2023-05-07 23:05:32 +0800

    用队列实现栈

--- suanfa/5.stack-queue/2.用队列实现栈.go
+++ suanfa/5.stack-queue/2.用队列实现栈.go
@@ -1,1 +1,46 @@
 package main
+
+import "fmt"
+
+type MyStack struct {
+	Queue []int
+}
+
+func Constructor() MyStack {
+	return MyStack{
+		Queue: make([]int, 0),
+	}
+}
+
+func (this *MyStack) Push(x int) {
+	this.Queue = append(this.Queue, x)
+}
+
+func (this *MyStack) Pop() int {
+	val := this.Queue[len(this.Queue)-1]
+	this.Queue = this.Queue[0 : len(this.Queue)-1]
+	return val
+}
+
+func (this *MyStack) Top() int {
+	return this.Queue[len(this.Queue)-1]
+}
+
+func (this *MyStack) Empty() bool {
+	return len(this.Queue) == 0
+}
+
+func main() {
+	stack := Constructor()
+	fmt.Println(stack)
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	fmt.Println(stack)
+	fmt.Println(stack.Top())
+	fmt.Println(stack)
+	fmt.Println(stack.Pop())
+	fmt.Println(stack)
+	fmt.Println(stack.Empty())
+}
