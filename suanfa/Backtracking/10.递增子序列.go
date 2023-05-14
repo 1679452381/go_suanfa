c37088b95fa27c6adad10e3bd5649de9976a7a8e
zxc <15660589213@163.com>
2023-05-15 00:07:54 +0800

    递增子序列

--- suanfa/Backtracking/10.递增子序列.go
+++ suanfa/Backtracking/10.递增子序列.go
@@ -5,35 +5,31 @@ import "fmt"
 func findSubsequences(nums []int) [][]int {
 	res := make([][]int, 0)
 	path := make([]int, 0)
-	used := make([]bool, len(nums))
-	findSubsequencesBack(nums, &res, path, 0, used)
+
+	findSubsequencesBack(nums, &res, path, 0)
 	return res
 }
 
-func findSubsequencesBack(nums []int, res *[][]int, path []int, start int, used []bool) {
+func findSubsequencesBack(nums []int, res *[][]int, path []int, start int) {
 	if len(path) > 1 {
 		temp := make([]int, len(path))
 		copy(temp, path)
 		*res = append(*res, temp)
 	}
+	used := make(map[int]bool)
 	for i := start; i < len(nums); i++ {
-		if i > 0 && nums[i] == nums[i-1] && used[i-1] != true {
+		//if i > 0 && nums[i] == nums[i-1] && used[i-1] != true {
+		//	continue
+		//}
+		if used[nums[i]] {
 			continue
 		}
-		if len(path) == 0 {
+		if len(path) == 0 || nums[i] >= path[len(path)-1] {
 			path = append(path, nums[i])
-			used[i] = true
-		} else {
-			if nums[i] >= path[len(path)-1] {
-				path = append(path, nums[i])
-				used[i] = true
-			} else {
-				return
-			}
+			used[nums[i]] = true
+			findSubsequencesBack(nums, res, path, i+1)
+			path = path[:len(path)-1]
 		}
-		findSubsequencesBack(nums, res, path, i+1, used)
-		path = path[:len(path)-1]
-		used[i] = false
 	}
 
 }
