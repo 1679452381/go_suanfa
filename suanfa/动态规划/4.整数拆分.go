8ad4a04eb4d6b64a83a084ab9e9c963009d8025f
zxc <15660589213@163.com>
2023-05-20 22:45:25 +0800

    动态规划：整数拆分

--- suanfa/动态规划/4.整数拆分.go
+++ suanfa/动态规划/4.整数拆分.go
@@ -21,9 +21,14 @@ func integerBreak(n int) int {
 	dp := make([]int, n+1)
 	for i := 3; i <= n; i++ {
 		for j := 1; j <= i/2; j++ {
-			dp[i] = _max(dp[i], _max(dp[i-j]*j, (i-j)*j))
+			dp[i] = _max(dp[i], _max(dp[i-j]*j, (i-j)*j)) //[0 1 1 2 4 6 9 12 18 27 36 54 81 108]
+			//[0 1 2 3 4 5 6 7  8  9  10 11 12 13 ]
+			//dp[i] = _max(dp[i-j]*j, (i-j)*j)            //[0 0 0 2 4 6 9 12 16 24 30 45 54 72]
+
 		}
 	}
+	fmt.Println(dp)
+	println(dp)
 	return dp[n]
 }
 
