6f1915e32a1729e15be63d7f18c6b654c749599b
zxc <15660589213@163.com>
2023-06-21 16:17:59 +0800

    动态规划

---
+++ suanfa/动态规划/23.最大礼物价值.go
@@ -0,0 +1,45 @@
+/**
+ * @author X
+ * @date 2023/6/21
+ * @description
+ **/
+package main
+
+import "fmt"
+
+func maxValue(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = grid[i][0] + dp[i-1][0]
+	}
+	for j := 1; j < n; j++ {
+		dp[0][j] = grid[0][j] + dp[0][j-1]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = grid[i][j] + max1(dp[i-1][j], dp[i][j-1])
+		}
+	}
+	fmt.Println(dp)
+	return dp[m-1][n-1]
+}
+func max1(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+
+	fmt.Println(maxValue([][]int{
+		{1, 3, 1},
+		{1, 5, 1},
+		{4, 2, 1},
+	}))
+}
